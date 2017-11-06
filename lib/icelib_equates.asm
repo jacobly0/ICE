3b1fe8944809fa0e9d361c8147c5e3bec2154a6e
PT_ <petertillema@solcon.nl>
2017-11-06 17:25:57 +0100

    Remove obj files

--- lib/icelib_equates.asm
+++
@@ -1,5 +0,0 @@
- .db 192,"icelib",0,1
-_ice_Compile:
- jp 0
-_ice_Register:
- jp 3
--- lib/icelib_header.asm
+++
@@ -1,7 +0,0 @@
- define .icelib_header,space=ram
- define .icelib,space=ram
- segment .icelib_header
- .assume adl=1
- db 192,"ICELIB",0,1
- end
-
