6d1f3dba66f37e7a53c790fe433afd73647a5431
PeterTillema <petertillema@solcon.nl>
2018-03-12 14:08:51 +0100

    Switch to other JS variable

--- src/ice_sc.c
+++ src/ice_sc.c
@@ -18,7 +18,7 @@ prescan_t prescan;
 bool ice_open(char tempName[9]) {
     // Load the data and reset the pointer
     bool ret = EM_ASM_({
-        return compiler.openProg($0, $1, $2, $3);
+        return ICEcompiler.openProg($0, $1, $2, $3);
     }, tempName, ice.progInputData, &ice.programLength, ice.progInputPtr);
 
     if (ret) {
@@ -36,7 +36,7 @@ void ice_open_first_prog(void) {
     memset(&reg, 0, sizeof(reg_t));
     memset(&prescan, 0, sizeof(prescan_t));
     EM_ASM_({
-        compiler.openFirstProg($0, $1);
+        ICEcompiler.openFirstProg($0, $1);
     }, ice.progInputData, &ice.programLength);
 
     ice.progInputPtr = 0;
@@ -47,19 +47,19 @@ void ice_open_first_prog(void) {
 
 void ice_close(void) {
     EM_ASM_({
-        compiler.closeProg($0, $1, $2);
+        ICEcompiler.closeProg($0, $1, $2);
     }, ice.progInputData, &ice.programLength, &ice.progInputPtr);
 }
 
 void ice_error(char *error, uint24_t currentLine) {
     EM_ASM_({
-        compiler.error($0, $1);
+        ICEcompiler.error($0, $1);
     }, error, currentLine);
 }
 
 void ice_export(uint8_t *outputPtr, uint24_t size) {
     EM_ASM_({
-        compiler.exportProgram($0, $1, $2);
+        ICEcompiler.exportProgram($0, $1, $2);
     }, ice.outName, outputPtr, size);
 }
 
